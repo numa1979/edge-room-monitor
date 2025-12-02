378a35200905622a38a58f5a0f32ba95fd5f599e
numa1979 <t.numamura@innoxia.co.jp>
2025-12-03 01:00:50 +0900

    軽量化

--- jetson_setup_scripts/modules/install_host_packages.sh
+++ jetson_setup_scripts/modules/install_host_packages.sh
@@ -3,11 +3,8 @@ set -euo pipefail
 
 sudo apt update
 sudo apt install -y \
-  build-essential dkms git curl wget \
+  build-essential dkms git curl \
   ca-certificates libnss3 libnss3-tools \
-  python3 python3-venv python3-pip \
-  libgl1 \
-  chromium-codecs-ffmpeg-extra \
   avahi-daemon
 
 # mDNS で <hostname>.local へ名前解決できるようにする
